8516d0d0db71b57b5fa206104762a22b5db27d83
mdmominul-310 <mdmominulislam310@gmail.com>
2021-10-05 23:41:54 +0600

    final

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,20 +1,22 @@
 import { Row } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
 import useCourse from '../../Hooks/useCourseData';
-import Course from './Course/Course';
-const Courses = (props) => {
-    const[courses]=useCourse();
-    return (
-        <div className="my-5 container">
-            <h2 className="text-center my-5">Courses</h2>
-            <Row>
-            {
-                courses.slice(0,4).map(course=><Course key={course.key} course={course} handleCart={props.handleCart} ></Course>)
-            }
-            </Row>
-            <NavLink to="/services" className="btn btn-success"> See All</NavLink>
+import Course from './Course/Course'
+const Services = (props) => {
+    
+  const[courses]=useCourse();
+     return (
+        <div className="container my-5">
+          <h4 className="my-5 text-center">All Services</h4>
+          <Row md={2} lg={3} className="g-4">
+         
+              {
+                courses.slice(0,6).map(course=><Course key={course.key} course={course} handleCart={props.handleCart}></Course>)
+              }
+          
+          </Row>
+
         </div>
     );
 };
 
-export default Courses;
+export default Services;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Courses from '../Courses/Courses';
 import Slider from '../Slider/Slider';
+import { NavLink } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 const Home = (props) => {
     return (
         <div>
             <Slider></Slider>
-            <Courses handleCart={props.handleCart}></Courses>
+        <Courses handleCart={props.handleCart}></Courses>
+        <Container className="mb-5">  
+        <NavLink className="btn btn-success c" to="/services">See All</NavLink>
+            </Container> 
+        
         </div>
+        
     );
 };
 
